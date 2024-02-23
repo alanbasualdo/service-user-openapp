f6d744d85e46c0cf90abc305a5ac096297ac3f5b
Alan Basualdo <alanbasualdo7@outlook.com>
2024-02-23 20:15:45 -0300

    auth

--- helpers/generateJWT.js
+++ helpers/generateJWT.js
@@ -23,35 +23,26 @@ const generateJWT = (_id = "") => {
 };
 
 const checkJWT = async (req, res, next) => {
-  const token = req;
-  console.log(req.header)
-
+  const { token } = req.headers;
   if (!token) {
     return res.status(401).json({
       message: "No hay token en la petición",
     });
   }
-
   try {
     const { _id } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
-
     const user = await User.findById(_id);
-    console.log(user)
     if (!user) {
       return res.status(401).json({
         message: "Usuario no existe",
       });
     }
-
-    // Verificar si el usuario tiene estado en true
     if (!user.state) {
       return res.status(401).json({
         message: "Token no válido",
       });
     }
-
     req.user = user;
-
     next();
   } catch (error) {
     console.log(error);
